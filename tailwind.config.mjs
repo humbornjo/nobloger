b127f8d2732493ac581d74075060b5a457c8fbde
humbornjo <humbornjo@gmail.com>
2024-09-03 22:21:18 +0800

    update: basic usage done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require('tailwindcss/plugin')
-
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -10,49 +8,5 @@ export default {
 		},
 		extend: {},
 	},
-	plugins: [
-		plugin(function({ addBase, theme }) {
-			addBase({
-				'h1': {
-					fontFamily: 'NanumGothic',
-					fontSize: theme('fontSize.2xl'),
-					fontWeight: '800',
-					marginBottom: '36px',
-					marginTop: '36px'
-				},
-				'h2': {
-					fontFamily: 'NanumGothic',
-					fontSize: theme('fontSize.xl'),
-					fontWeight: '700',
-					marginBottom: '24px',
-					marginTop: '24px'
-				},
-				'h3': {
-					fontFamily: 'NanumGothic',
-					fontWeight: '600',
-					fontSize: theme('fontSize.lg'),
-					marginBottom: '16px',
-					marginTop: '16px'
-				},
-				'pre': {
-					marginBottom: '10px',
-					marginTop: '10px',
-					padding: '1.5em',
-					borderRadius: '0.75em',
-				},
-				'code': {
-					fontFamily: 'Menlo',
-					fontSize: theme('fontSize.md'),
-					filter: 'brightness(.82)',
-				},
-				'p': {
-					fontFamily: 'NanumGothic',
-					fontSize: theme('fontSize.md'),
-					fontWeight: '200',
-					marginBottom: '8px',
-					marginTop: '8px'
-				},
-			})
-		})
-	],
+	plugins: [],
 }
