7068457b3a065f0b44fec651c0e2879012ab456e
humbornjo <92044720+humbornjo@users.noreply.github.com>
2024-09-05 01:45:58 +0800

    update: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://humbornjo.github.io',
-  base: '/nobloger',
+  base: 'nobloger',
 });
