e3c3b7f4235ff520870fd16d790d278a816aa7f8
humbornjo <humbornjo@gmail.com>
2024-09-04 11:56:16 +0800

    update: maybe Hack fit Chinese better

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
+			'sans': ['Hack'],
 			'mono': ['Monocraft', 'SFMono-Regular'],
 		},
 		extend: {},
